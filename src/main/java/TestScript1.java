c7d7c3a8a57f9b1b8d8567c75c904fc766ec811d
prabukumark <37550775+prabukumark@users.noreply.github.com>
2020-06-02 09:05:43 +0530

    Added Class and change POM
    Launch browser, Enter Url, Maximize browser and close browser

--- src/main/java/TestScript1.java
+++ src/main/java/TestScript1.java
@@ -1,8 +1,25 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestScript1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("New Test");
+		
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\Vidhya\\Downloads\\chromedriver_win32\\chromedriver.exe");
+			
+			//Object Creation for Chrome Driver class and launch browser
+			WebDriver move = new ChromeDriver();
+			
+			//Enter Url in the browser
+			move.get("https://ams.moveoutdoor.com.au/");
+			
+			//Maximize the browser
+			move.manage().window().maximize();
+			
+			//Close the browser
+			move.close();
+			
+			//Print message for our understanding
+			System.out.println("Launch browser, Enter Url, Maximize browser and close browser");
 	}
 }
